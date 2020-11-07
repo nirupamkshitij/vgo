c9f0c8183a3a84f08a22bd0cfc2aab6e1cf172a4
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-11-07 21:28:22 +0530

    app update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:vgo/screens/emaillogin.dart';
 import 'package:vgo/screens/login.dart';
 import 'package:vgo/screens/navigation.dart';
 import 'package:vgo/screens/profile.dart';
@@ -40,6 +41,7 @@ class _MainAppState extends State<MainApp> {
         'navigation': (context) => NavigationScreen(),
         'login': (context) => Login(),
         'profile': (context) => ProfileScreen(),
+        'email': (context) => EmailLogin(),
         // 'signin': (context) => SignIn(),
         // 'notification': (context) => NotificationScreen(),
         // 'search': (context) => SearchPage(),
@@ -47,7 +49,7 @@ class _MainAppState extends State<MainApp> {
         // 'home': (context) => HomeScreen(),
         // 'signin1': (context) => SignInData(),
         // 'terms': (context) => Terms(),
-        // 'email': (context) => EmailLogin(),
+        //
       },
     );
   }
---
+++ lib/screens/home.dart
@@ -0,0 +1,302 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:tik_tok_demo/screens/pages/videos.dart';
+import 'package:tik_tok_demo/utilities/constants.dart';
+import 'package:tik_tok_demo/widgets/bottomnavbar.dart';
+import 'package:vgo/utilities/constants.dart';
+import 'package:video_player/video_player.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+int index = 0;
+
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: bottomContainerColor,
+        bottomNavigationBar: Theme(
+          data: Theme.of(context).copyWith(
+              canvasColor: mainTextColor,
+              textTheme: Theme.of(context)
+                  .textTheme
+                  .copyWith(caption: TextStyle(color: Colors.yellow))),
+          child: BottomNavigationBar(
+            backgroundColor: Colors.black,
+            selectedItemColor: mainBgColor,
+            showSelectedLabels: false,
+            type: BottomNavigationBarType.fixed,
+            showUnselectedLabels: false,
+            unselectedItemColor: fadeTextColor,
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(icon: FaIcon(Icons.home), label: ''),
+              BottomNavigationBarItem(
+                  icon: FaIcon(FontAwesomeIcons.search), label: ''),
+              BottomNavigationBarItem(icon: customIcon(), label: ''),
+              BottomNavigationBarItem(
+                  icon: FaIcon(FontAwesomeIcons.solidHeart), label: ''),
+              BottomNavigationBarItem(icon: FaIcon(Icons.person), label: '')
+            ],
+            currentIndex: currentIndex,
+            onTap: (value) {
+              // changePage(value);
+              if (value == 0) {
+              } else if (value == 1) {
+                Navigator.pushNamed(context, 'search');
+              } else if (value == 2) {
+                Navigator.pushNamed(context, 'camera');
+              } else if (value == 3) {
+                Navigator.pushNamed(context, 'notification');
+              } else if (value == 4) {
+                Navigator.pushNamed(context, 'profile');
+              }
+            },
+          ),
+        ),
+        body: Stack(
+          children: [
+            DefaultTabController(
+              length: 2,
+              child: Scaffold(
+                backgroundColor: bottomContainerColor,
+                appBar: TabBar(
+                  indicatorColor: mainBgColor,
+                  indicatorSize: TabBarIndicatorSize.label,
+                  tabs: [
+                    Tab(
+                      child: Container(
+                          child: Text(
+                        'Following',
+                        style: GoogleFonts.josefinSans(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                          color: mainBgColor,
+                        ),
+                      )),
+                    ),
+                    Tab(
+                      child: Container(
+                          child: Text(
+                        'For You',
+                        style: GoogleFonts.josefinSans(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                          color: mainBgColor,
+                        ),
+                      )),
+                    ),
+                  ],
+                ),
+                body: TabBarView(
+                  children: [
+                    Container(
+                      height: size.height,
+                      color: bottomContainerColor,
+                      child: GridView.count(
+                        crossAxisCount: 2,
+                        children: List.generate(8, (index) {
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (_) => VideoPage(),
+                                ),
+                              );
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  width: 3.0,
+                                  color: bottomContainerColor,
+                                ),
+                              ),
+                              constraints: BoxConstraints.expand(height: 150),
+                              child: Stack(
+                                children: [
+                                  VideoItem(
+                                    followingURL[index],
+                                  ),
+                                  Align(
+                                    alignment: Alignment.bottomLeft,
+                                    child: Row(
+                                      mainAxisSize: MainAxisSize.max,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.end,
+                                      children: [
+                                        Container(
+                                          padding: EdgeInsets.only(
+                                              left: 5, bottom: 5),
+                                          child: Row(
+                                            children: [
+                                              ClipRRect(
+                                                borderRadius:
+                                                    BorderRadius.circular(
+                                                        100.0),
+                                                child: Image.network(
+                                                  'https://picsum.photos/id/${index + 50}/250/250',
+                                                  height: 30.0,
+                                                  width: 30.0,
+                                                ),
+                                              ),
+                                              Padding(
+                                                padding:
+                                                    EdgeInsets.only(left: 10.0),
+                                                child: Text(
+                                                  followingName[index],
+                                                  style: GoogleFonts.raleway(
+                                                    fontSize: 14,
+                                                    fontWeight: FontWeight.w700,
+                                                    color: mainBgColor,
+                                                  ),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        }),
+                      ),
+                    ),
+                    Container(
+                      height: size.height,
+                      color: bottomContainerColor,
+                      child: GridView.count(
+                        crossAxisCount: 2,
+                        children: List.generate(8, (index) {
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (_) => VideoPage(),
+                                ),
+                              );
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  width: 3.0,
+                                  color: bottomContainerColor,
+                                ),
+                              ),
+                              constraints: BoxConstraints.expand(height: 150),
+                              child: Stack(
+                                children: [
+                                  VideoItem(
+                                    forYouURL[index],
+                                  ),
+                                  Align(
+                                    alignment: Alignment.bottomLeft,
+                                    child: Row(
+                                      mainAxisSize: MainAxisSize.max,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.end,
+                                      children: [
+                                        Container(
+                                          padding: EdgeInsets.only(
+                                              left: 5, bottom: 5),
+                                          child: Row(
+                                            children: [
+                                              ClipRRect(
+                                                borderRadius:
+                                                    BorderRadius.circular(
+                                                        100.0),
+                                                child: Image.network(
+                                                  'https://picsum.photos/id/${index + 250}/250/250',
+                                                  height: 30.0,
+                                                  width: 30.0,
+                                                ),
+                                              ),
+                                              Padding(
+                                                padding:
+                                                    EdgeInsets.only(left: 10.0),
+                                                child: Text(
+                                                  forYouName[index],
+                                                  style: GoogleFonts.raleway(
+                                                    fontSize: 14,
+                                                    fontWeight: FontWeight.w700,
+                                                    color: mainBgColor,
+                                                  ),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        }),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class VideoItem extends StatefulWidget {
+  final String url;
+
+  VideoItem(this.url);
+  @override
+  _VideoItemState createState() => _VideoItemState();
+}
+
+class _VideoItemState extends State<VideoItem> {
+  VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(widget.url)..initialize();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: _controller.value.initialized
+          ? Stack(
+              children: [
+                AspectRatio(
+                  aspectRatio: MediaQuery.of(context).size.width *
+                      2 /
+                      MediaQuery.of(context).size.height,
+                  child: VideoPlayer(_controller),
+                )
+              ],
+            )
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
