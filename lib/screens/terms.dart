48292dde3eeed946c74c1a9bf9e12469d4bb0e10
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-11-08 17:53:32 +0530

    app update

--- lib/screens/terms.dart
+++ lib/screens/terms.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:tik_tok_demo/utilities/constants.dart';
+import 'package:vgo/utilities/constants.dart';
 
 class Terms extends StatelessWidget {
   @override
