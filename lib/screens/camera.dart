7090e59abeeb3cfc8549989e4b3fecd54202dede
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-11-09 11:59:22 +0530

    app update

--- lib/screens/camera.dart
+++ lib/screens/camera.dart
@@ -327,6 +327,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:vgo/pages/video-controls.dart';
 
 enum CameraMode {
   PhotosMode,
