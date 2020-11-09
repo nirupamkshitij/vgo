415785dbb59be9a7cb242e701fcea43df596b1a3
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-11-09 11:29:58 +0530

    app update

--- lib/pages/video-create.dart
+++ lib/pages/video-create.dart
@@ -1,5 +1,7 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:vgo/screens/camera.dart';
 
 class CreateVideo extends StatefulWidget {
@@ -12,7 +14,7 @@ String path;
 class _CreateVideoState extends State<CreateVideo> {
   CameraController _camController;
   Future<void> initializeControllerFuture;
-
+  bool isDisabled = false;
   @override
   void initState() {
     super.initState();
@@ -41,8 +43,45 @@ class _CreateVideoState extends State<CreateVideo> {
               Align(
                 alignment: Alignment.bottomCenter,
                 child: !_camController.value.isRecordingVideo
-                    ? RawMaterialButton(onPressed: null)
-                    : RawMaterialButton(onPressed: null),
+                    ? RawMaterialButton(
+                        onPressed: () async {
+                          try {
+                            await initializeControllerFuture;
+                            path = join(
+                                (await getApplicationDocumentsDirectory()).path,
+                                '${DateTime.now()}.mp4');
+                            setState(() {
+                              _camController.startVideoRecording(path);
+                              isDisabled = true;
+                              isDisabled = !isDisabled;
+                            });
+                          } catch (e) {
+                            print(e);
+                          }
+                        },
+                        child: Icon(
+                          Icons.camera_alt_rounded,
+                          size: 50,
+                          color: Colors.white,
+                        ),
+                        padding: EdgeInsets.all(10.0),
+                        shape: CircleBorder(),
+                      )
+                    : null,
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: _camController.value.isRecordingVideo
+                    ? RawMaterialButton(onPressed: () {
+                        setState(() {
+                          if (_camController.value.isRecordingVideo) {
+                            _camController.stopVideoRecording();
+                            isDisabled = false;
+                            isDisabled = !isDisabled;
+                          }
+                        });
+                      })
+                    : null,
               )
             ],
           );
