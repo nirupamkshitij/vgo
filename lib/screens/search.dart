7cca89fbcb71c9e743bc39f4810ac1effec2ce4c
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-11-20 11:43:12 +0530

    app update

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -272,66 +272,73 @@ class _SearchPageState extends State<SearchPage> {
                       ),
                       constraints: BoxConstraints.expand(height: 150),
                       child: GridView.count(
-                        crossAxisCount: 2,
-                        children: List.generate(
-                            gotVideos ? videoData.length : 1, (index) {
+                        crossAxisCount: 1,
+                        children:
+                            List.generate(gotVideos ? tags.length : 1, (index) {
                           return gotVideos
-                              ? GestureDetector(
-                                  child: Stack(
-                                    children: [
-                                      _futreImage[index],
-                                      Align(
-                                        alignment: Alignment.bottomLeft,
-                                        child: Row(
-                                          mainAxisSize: MainAxisSize.max,
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.end,
-                                          children: [
-                                            Container(
-                                              padding: EdgeInsets.only(
-                                                  left: 5, bottom: 5),
-                                              child: Row(
-                                                children: [
-                                                  ClipRRect(
-                                                    borderRadius:
-                                                        BorderRadius.circular(
-                                                            100.0),
-                                                    child: Image.network(
-                                                      videoData[index]['dp'],
-                                                      height: 30.0,
-                                                      width: 30.0,
-                                                    ),
-                                                  ),
-                                                  Padding(
-                                                    padding: EdgeInsets.only(
-                                                        left: 10.0),
-                                                    child: Text(
-                                                      videoData[index]
-                                                          ['userId'],
-                                                      style:
-                                                          GoogleFonts.raleway(
-                                                        fontSize: 14,
-                                                        fontWeight:
-                                                            FontWeight.w700,
-                                                        color: mainBgColor,
+                              ? Column(
+                                  children: [
+                                    GestureDetector(
+                                      child: Stack(
+                                        children: [
+                                          _futreImage[index],
+                                          Align(
+                                            alignment: Alignment.bottomLeft,
+                                            child: Row(
+                                              mainAxisSize: MainAxisSize.max,
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.end,
+                                              children: [
+                                                Container(
+                                                  padding: EdgeInsets.only(
+                                                      left: 5, bottom: 5),
+                                                  child: Row(
+                                                    children: [
+                                                      ClipRRect(
+                                                        borderRadius:
+                                                            BorderRadius
+                                                                .circular(
+                                                                    100.0),
+                                                        child: Image.network(
+                                                          videoData[index]
+                                                              ['dp'],
+                                                          height: 30.0,
+                                                          width: 30.0,
+                                                        ),
                                                       ),
-                                                    ),
+                                                      Padding(
+                                                        padding:
+                                                            EdgeInsets.only(
+                                                                left: 10.0),
+                                                        child: Text(
+                                                          videoData[index]
+                                                              ['userId'],
+                                                          style: GoogleFonts
+                                                              .raleway(
+                                                            fontSize: 14,
+                                                            fontWeight:
+                                                                FontWeight.w700,
+                                                            color: mainBgColor,
+                                                          ),
+                                                        ),
+                                                      ),
+                                                    ],
                                                   ),
-                                                ],
-                                              ),
+                                                ),
+                                              ],
                                             ),
-                                          ],
-                                        ),
+                                          ),
+                                        ],
                                       ),
-                                    ],
-                                  ),
-                                  onTap: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => VideoPage(
-                                                videoData: videoData)));
-                                  },
+                                      onTap: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) => VideoPage(
+                                                    videoData: videoData)));
+                                      },
+                                    )
+                                  ],
                                 )
                               : Container(
                                   color: bottomContainerColor,
