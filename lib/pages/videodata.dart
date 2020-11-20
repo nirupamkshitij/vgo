7f7ed147decead723b54853b8fe5f5ec447fbe55
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-11-20 14:16:26 +0530

    app update

--- lib/pages/videodata.dart
+++ lib/pages/videodata.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:vgo/utilities/constants.dart';
 
@@ -13,26 +15,22 @@ class _VideoDetailsDataState extends State<VideoDetailsData> {
   final _nameController = TextEditingController();
   final _songController = TextEditingController();
   final _artistController = TextEditingController();
+  final _tagsController = TextEditingController();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   _dataUpdates() async {
     try {
       final result = await InternetAddress.lookup('google.com');
       if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
         try {
-          if (userMail == '' ||
-              userBio == '' ||
-              userName == '' ||
-              userId == '' ||
-              userNumber == 0 ||
-              userURL == '' ||
-              userImage == null) {
-            print(userMail);
-            print(userBio);
-            print(userName);
-            print(userId);
-            print(userNumber);
-            print(userURL);
-            print(userImage);
+          if (_nameController.text == '' ||
+              _songController.text == '' ||
+              _artistController == '' ||
+              _tagsController.text == '' ||
+              _nameController.text == null ||
+              _songController.text == null ||
+              _artistController == null ||
+              _tagsController.text == null) {
             _scaffoldKey.currentState.showSnackBar(
               SnackBar(
                 backgroundColor: errorCardColor,
@@ -126,120 +124,125 @@ class _VideoDetailsDataState extends State<VideoDetailsData> {
   }
 
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        color: bottomContainerColor,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(
-                top: 25,
-                left: 25,
-                right: 25,
-              ),
-              child: TextFormField(
-                controller: _nameController,
-                onChanged: (value) {
-                  setState(() {});
-                },
-                style: TextStyle(
-                  fontSize: 16,
-                  color: mainBgColor,
-                  fontWeight: FontWeight.w600,
-                ),
-                decoration: InputDecoration(
-                  labelStyle: TextStyle(
-                    color: darkFadeTextColor,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
+    return SafeArea(
+      child: Scaffold(
+        key: _scaffoldKey,
+        body: SingleChildScrollView(
+          child: Container(
+            color: bottomContainerColor,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 25,
+                    left: 25,
+                    right: 25,
                   ),
-                  border: InputBorder.none,
-                  labelText: 'Video Name',
-                ),
-              ),
-            ),
-            Padding(
-                padding: EdgeInsets.only(
-                  top: 25,
-                  left: 25,
-                  right: 25,
-                ),
-                child: TextFormField(
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: mainBgColor,
-                    fontWeight: FontWeight.w600,
-                  ),
-                  decoration: InputDecoration(
-                    labelStyle: TextStyle(
-                      color: darkFadeTextColor,
+                  child: TextFormField(
+                    controller: _nameController,
+                    onChanged: (value) {
+                      setState(() {});
+                    },
+                    style: TextStyle(
                       fontSize: 16,
+                      color: mainBgColor,
                       fontWeight: FontWeight.w600,
                     ),
-                    border: InputBorder.none,
-                    labelText: 'Video Artist',
+                    decoration: InputDecoration(
+                      labelStyle: TextStyle(
+                        color: darkFadeTextColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      border: InputBorder.none,
+                      labelText: 'Video Name',
+                    ),
                   ),
-                )),
-            Padding(
-              padding: EdgeInsets.only(
-                top: 25,
-                left: 25,
-                right: 25,
-              ),
-              child: TextFormField(
-                controller: _songController,
-                keyboardType: TextInputType.visiblePassword,
-                obscureText: true,
-                onChanged: (value) {
-                  setState(() {});
-                },
-                style: TextStyle(
-                  fontSize: 16,
-                  color: mainBgColor,
-                  fontWeight: FontWeight.w600,
                 ),
-                decoration: InputDecoration(
-                  labelStyle: TextStyle(
-                    color: darkFadeTextColor,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
+                Padding(
+                    padding: EdgeInsets.only(
+                      top: 25,
+                      left: 25,
+                      right: 25,
+                    ),
+                    child: TextFormField(
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: mainBgColor,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      decoration: InputDecoration(
+                        labelStyle: TextStyle(
+                          color: darkFadeTextColor,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        border: InputBorder.none,
+                        labelText: 'Video Artist',
+                      ),
+                    )),
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 25,
+                    left: 25,
+                    right: 25,
                   ),
-                  border: InputBorder.none,
-                  labelText: 'Song',
+                  child: TextFormField(
+                    controller: _songController,
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    onChanged: (value) {
+                      setState(() {});
+                    },
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: mainBgColor,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    decoration: InputDecoration(
+                      labelStyle: TextStyle(
+                        color: darkFadeTextColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      border: InputBorder.none,
+                      labelText: 'Song',
+                    ),
+                  ),
                 ),
-              ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 25,
+                    left: 25,
+                    right: 25,
+                  ),
+                  child: TextFormField(
+                    controller: _artistController,
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    onEditingComplete: () {
+                      setState(() {});
+                    },
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: mainBgColor,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    decoration: InputDecoration(
+                      labelStyle: TextStyle(
+                        color: darkFadeTextColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      border: InputBorder.none,
+                      labelText: 'tags',
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Padding(
-              padding: EdgeInsets.only(
-                top: 25,
-                left: 25,
-                right: 25,
-              ),
-              child: TextFormField(
-                controller: _artistController,
-                keyboardType: TextInputType.visiblePassword,
-                obscureText: true,
-                onEditingComplete: () {
-                  setState(() {});
-                },
-                style: TextStyle(
-                  fontSize: 16,
-                  color: mainBgColor,
-                  fontWeight: FontWeight.w600,
-                ),
-                decoration: InputDecoration(
-                  labelStyle: TextStyle(
-                    color: darkFadeTextColor,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
-                  ),
-                  border: InputBorder.none,
-                  labelText: 'tags',
-                ),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
