4be3a881c64b686d8d29009e835232e51a57738e
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-11-20 13:21:03 +0530

    app update

--- lib/screens/camera.dart
+++ lib/screens/camera.dart
@@ -7,8 +7,7 @@ import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:vgo/screens/dataupload.dart';
-import 'package:vgo/screens/search.dart';
+import 'package:vgo/pages/dataupload.dart';
 
 class CameraScreen extends StatefulWidget {
   const CameraScreen({Key key}) : super(key: key);
