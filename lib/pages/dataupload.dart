09a9f6617f9d868462d6aac64032f29decc14847
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-11-20 13:17:56 +0530

    app update

--- lib/pages/dataupload.dart
+++ lib/pages/dataupload.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:vgo/utilities/constants.dart';
 import 'package:video_player/video_player.dart';
@@ -65,56 +64,107 @@ class _VideoUploadDataState extends State<VideoUploadData> with RouteAware {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Stack(
-      children: [
-        ClipRect(
-          child: Container(
-            child: Transform.scale(
-              scale: _controller.value.aspectRatio / size.aspectRatio,
-              child: Center(
-                child: AspectRatio(
-                  aspectRatio: _controller.value.aspectRatio,
-                  child: FutureBuilder(
-                    future: _initializeVideoPlayerFuture,
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.done) {
-                        return AspectRatio(
-                          aspectRatio: size.aspectRatio,
-                          child: VideoPlayer(_controller),
-                        );
-                      } else {
-                        return Center(child: CircularProgressIndicator());
-                      }
-                    },
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: mainBgColor,
+            size: 32,
+          ),
+          leading: GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Padding(
+              padding: EdgeInsets.only(
+                left: 15,
+                top: 10,
+              ),
+              child: Icon(
+                (Icons.arrow_back_ios),
+                size: 28,
+              ),
+            ),
+          ),
+          actions: [
+            GestureDetector(
+              onTap: () {
+                // Navigator.of(context).pushNamed('search');
+              },
+              child: Padding(
+                padding: EdgeInsets.only(
+                  right: 15,
+                  top: 10,
+                ),
+                child: Icon(Icons.star_rate_rounded),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushNamed('search');
+              },
+              child: Padding(
+                padding: EdgeInsets.only(
+                  right: 15,
+                  top: 10,
+                ),
+                child: Icon(Icons.search),
+              ),
+            ),
+          ]),
+      body: Stack(
+        children: [
+          ClipRect(
+            child: Container(
+              child: Transform.scale(
+                scale: _controller.value.aspectRatio / size.aspectRatio,
+                child: Center(
+                  child: AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    child: FutureBuilder(
+                      future: _initializeVideoPlayerFuture,
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.done) {
+                          return AspectRatio(
+                            aspectRatio: size.aspectRatio,
+                            child: VideoPlayer(_controller),
+                          );
+                        } else {
+                          return Center(child: CircularProgressIndicator());
+                        }
+                      },
+                    ),
                   ),
                 ),
               ),
             ),
           ),
-        ),
-        Padding(
-          padding: EdgeInsets.only(
-            top: 25,
-            right: 25,
-          ),
-          child: Align(
-            alignment: Alignment.topRight,
-            child: FlatButton(
-              onPressed: () {
-                // _dataUpdates();
-                // Navigator.popAndPushNamed(context, 'home');
-              },
-              child: Text(
-                'Save',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
+          Padding(
+            padding: EdgeInsets.only(
+              top: 25,
+              right: 25,
+            ),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: FlatButton(
+                onPressed: () {
+                  // _dataUpdates();
+                  // Navigator.popAndPushNamed(context, 'home');
+                },
+                child: Text(
+                  'Save',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
               ),
             ),
-          ),
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 
