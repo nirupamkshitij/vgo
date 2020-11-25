e066b8c9dc5332138f096cd5237690f9651b8282
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-11-25 18:13:04 +0530

    app update

--- lib/pages/videos.dart
+++ lib/pages/videos.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
